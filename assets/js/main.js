4b8bb45db0fa84ec6e08fb0b36b8425e1fd19fe6
kirill-kundik <goliathusua@gmail.com>
2018-08-21 20:05:10 +0300

    fixed playing page animation while page is loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,53 @@
 $(window).bind("load", function () {
     // code goes here
     $('body').addClass('loaded');
-});
 
-$(function () {
+    $(function () {
 
-    setTimeout(function () {
-        $('.bgimg-1 > .caption > .border').fadeOut(1500, function () {
-            $(this).text('Я ТВІЙ ПУТІВНИК МОГИЛЯНКОЮ');
-            $(this).fadeIn(1500, function () {
-                $('.bgimg-1 > .caption > .button2').hide().fadeIn(1500);
-            });
-
-        })
-    }, 2500);
-
-
-    var container = $('html, body');
-    $('a').click(function () {
-        var target = $($.attr(this, 'href'));
         setTimeout(function () {
-            container.animate({
-                scrollTop: container.scrollTop() + target.offset().top
-            }, 1000);
-        }, 150);
-        return false;
-    });
+            $('.bgimg-1 > .caption > .border').fadeOut(1500, function () {
+                $(this).text('Я ТВІЙ ПУТІВНИК МОГИЛЯНКОЮ');
+                $(this).fadeIn(1500, function () {
+                    $('.bgimg-1 > .caption > .button2').hide().fadeIn(1500);
+                });
 
-    var isToggled = false;
+            })
+        }, 2500);
 
-    var $hamburger = $(".hamburger");
-    $hamburger.click(function (e) {
-        console.log('clicked');
-        $hamburger.toggleClass("is-active");
-        $('#myDropdown').toggle('show', function () {
-            isToggled = !isToggled;
+
+        var container = $('html, body');
+        $('a').click(function () {
+            var target = $($.attr(this, 'href'));
+            setTimeout(function () {
+                container.animate({
+                    scrollTop: container.scrollTop() + target.offset().top
+                }, 1000);
+            }, 150);
+            return false;
         });
-        // Do something else, like open/close menu
-    });
 
-    $(window).click(function (e) {
-        if (!e.target.classList.contains('dropbtn')) {
-            if (isToggled) {
-                $('#myDropdown').toggle('show');
-                isToggled = false;
+        var isToggled = false;
+
+        var $hamburger = $(".hamburger");
+        $hamburger.click(function (e) {
+            console.log('clicked');
+            $hamburger.toggleClass("is-active");
+            $('#myDropdown').toggle('show', function () {
+                isToggled = !isToggled;
+            });
+            // Do something else, like open/close menu
+        });
+
+        $(window).click(function (e) {
+            if (!e.target.classList.contains('dropbtn')) {
+                if (isToggled) {
+                    $('#myDropdown').toggle('show');
+                    isToggled = false;
+                }
+                $hamburger.toggleClass("is-active", false);
             }
-            $hamburger.toggleClass("is-active", false);
-        }
+        });
+
     });
 
 });
